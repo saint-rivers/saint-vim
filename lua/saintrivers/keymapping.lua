601ea1f1bdef894fcc01d9880365eb62ade92896
dayan <eam.dayan@gmail.com>
2023-02-21 00:36:39 +0700

    first commit

---
+++ lua/saintrivers/keymapping.lua
@@ -0,0 +1,11 @@
+return {
+    bind_keymap = function (mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+}
+
